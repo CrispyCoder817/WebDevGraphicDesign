f8552c684186fe7db19202a79e3b022e5153b572
Brandon G.M <brandongmerritt@gmail.com>
2021-07-21 14:33:58 -0500

    added graphicdesignpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './Pages/Home/';
+import GraphicDesign from './Pages/Graphics';
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/graphicdesign' component={GraphicDesign}/>
+
       </Switch>
     </Router>
   );
---
+++ src/Pages/Graphics/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function GraphicDesign() {
+    return (
+        <div>
+            <h1>GraphicDesignPage</h1>
+        </div>
+    )
+}
+
+export default GraphicDesign;
