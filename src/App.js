7b203a05c86fe8692b079461bd824b82b8497fe3
Brandon G.M <brandongmerritt@gmail.com>
2021-07-21 14:45:38 -0500

    about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './Pages/Home/';
+import About from './Pages/About';
 import GraphicDesign from './Pages/Graphics';
+import WebDev from './Pages/WebDev';
 
 
 function App() {
@@ -9,8 +11,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
         <Route exact path='/graphicdesign' component={GraphicDesign}/>
-
+        <Route exact path='/webdev' component={WebDev}/>
       </Switch>
     </Router>
   );
---
+++ src/Pages/WebDev/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+function WebDev() {
+    return (
+        <div>
+            <h1>WebDevPage</h1>
+        </div>
+    )
+}
+
+export default WebDev;
