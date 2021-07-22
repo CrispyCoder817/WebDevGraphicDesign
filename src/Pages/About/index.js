a0292bee0a28def306d2c2438873abc91d887d7e
Brandon G.M <brandongmerritt@gmail.com>
2021-07-22 14:34:49 -0500

    about02

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import Header from '../../Components/Header';
+import './styles.css';
 // import { Link } from 'react-router-dom';
 
 function About() {
     return (
         <div>
             <Header/>
-            <h1>About</h1>
-            <p> Born and raised in Alma, Michigan.<br></br>
-                Central Michigan University Alumnus with a Bachelor's of Integrative Public Relations and minor of Public Affairs - 2017.<br></br>
-                Full Stack Web Development (JavaScript) Certificate - 2021. <br></br>
+            <h1 className='margins highlighted'>About</h1>
+            <p className='margins'> Hometown: Alma, Michigan.<br></br>
+                Full Stack Web Dev (JavaScript) Certificate - 2021. <br></br>
+                Central Michigan University Alumnus<br></br>
+                B.A.A: Integrative Public Relations Minor: Public Affairs - 2017.<br></br>
                 Senior Graphic Designer for Three Rivers Media - August 2017. <br></br>
-                Freelance graphic designer since 2016.<br></br>
-                Photography is a hobby.<br></br>            
+                Freelance Graphic Designer - 2016.<br></br>
+                Photography - hobby.<br></br>            
             </p>
-            <h2>Skills</h2>
+            <h2 className='margins highlighted'>Skills</h2>
+            <h3 className='margins'>Graphic Design</h3>
             <ul>
-                <h3>Graphic Design</h3>
                 <li>Indesign</li>
                 <li>Photoshop</li>
                 <li>Illustrator</li>
                 <li>Photography</li>
                 <li>Social Media</li>
                 <li>Typography</li>
-
-                <h3>WebDev</h3>
+            </ul>
+                <h3 className='margins'>WebDev</h3>
+            <ul>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
@@ -32,8 +35,9 @@ function About() {
                 <li>Git/GitHub</li>
                 <li>Trello</li>
                 <li>Figma</li>
-
-                <h3>General</h3>
+            </ul>
+                <h3 className='margins'>General</h3>
+            <ul>
                 <li>Project Management</li>
                 <li>Positive Mental Attitude</li>
                 <li>Adaptability</li>
